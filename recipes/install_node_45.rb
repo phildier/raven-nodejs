82a15f31aecd96c35869ec93a469a510de2d5178
Phil Dier <phil@raventools.com>
2016-08-17 15:33:42 -0500

    use correct repo name

--- recipes/install_node_45.rb
+++ recipes/install_node_45.rb
@@ -4,14 +4,16 @@ end
 
 # install node repository for latest nodejs rpms
 package "nodesource-release" do
-	notifies :makecache, "yum_repository[nodesource-el]", :immediately
+	notifies :makecache, "yum_repository[nodesource]", :immediately
 end
 
 # ensure yum cache is updated
-yum_repository "nodesource-el" do
+yum_repository "nodesource" do
 	action :nothing
 end
 
 package "nodejs" do
 	version "4.5.0-1nodesource.el6"
 end
+
+#package "node-gyp"
