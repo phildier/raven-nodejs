bdb6113327e448f761e1065b7f5fb81bca5d7559
Phil Dier <phil@raventools.com>
2016-12-13 09:52:35 -0600

    optionally install node 6.9

--- metadata.rb
+++ metadata.rb
@@ -8,10 +8,24 @@ version          '0.1.0'
 
 depends "raven-deploy"
 
-recipe "raven-nodejs::default", "installs default nodejs version"
+recipe "raven-nodejs::default", "default tasks"
+recipe "raven-nodejs::install_node", "installs selected nodejs version"
 recipe "raven-nodejs::install_node_10", "installs nodejs 0.10"
 recipe "raven-nodejs::install_node_45", "installs nodejs 4.5.0"
+recipe "raven-nodejs::install_node_59", "installs nodejs 6.9.2"
 recipe "raven-nodejs::install_bower", "installs bower"
 recipe "raven-nodejs::install_mocha", "installs mocha"
 recipe "raven-nodejs::install_phantomjs", "installs phantomjs"
 recipe "raven-nodejs::install_devtools", "installs development tools"
+
+attribute "raven_nodejs",
+	:display_name => "Raven NodeJS",
+	:type => "hash"
+
+attribute "raven_nodejs/version",
+	:display_name => "NodeJS Version",
+	:description => "Version of NodeJS to install",
+	:required => "recommended",
+	:type => "string",
+	:recipes => ["raven-nodejs::install_node"],
+	:default => "4.5"
