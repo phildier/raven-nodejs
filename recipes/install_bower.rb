6f8da7c8fb80332bcb88e26887a4b43a95d5ebed
Byron Albert <byron@raventools.com>
2018-06-04 14:31:23 -0500

    update two more packages

--- recipes/install_bower.rb
+++ recipes/install_bower.rb
@@ -1,1 +1,10 @@
-package "node-bower"
+
+if node['platform_version'].to_f > 7.0
+	bash "install-bower-npn" do
+	code <<-EOH
+	npm install bower
+	EOH
+	end
+else
+  package "node-bower"
+end
--- recipes/install_phantomjs.rb
+++ recipes/install_phantomjs.rb
@@ -1,3 +1,12 @@
-package "node-phantomjs" do
-	version "2.1.7-1"
+
+if node['platform_version'].to_f > 7.0
+	bash "install-phantomjs-npn" do
+	code <<-EOH
+	npm install phantomjs
+	EOH
+	end
+else
+	package "node-phantomjs" do
+		version "2.1.7-1"
+	end
 end
